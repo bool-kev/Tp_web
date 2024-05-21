12258a50d20b95efdf1c9ad2e6742c072beafea7
bool-kev <155892753+bool-kev@users.noreply.github.com>
2024-05-21 01:25:38 +0000

    Delete web directory

--- web/userApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-app_name="user"
-urlpatterns = [
-    path('',home,name='home'),
-    path('<slug:matiere>/',matiere,name='matiere.vue'),
-    path('<slug:matiere>/<slug:cours>/',cours,name='cour.vue'),
-]
